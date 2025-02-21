342847cdc57132eb2e8d962fbe346f3be08348ce
unknown <jvanderlith@gmail.com>
2025-02-21 10:12:23 +0200

    Added method to find order by ID

--- src/main/java/com/example/store/repository/OrderRepository.java
+++ src/main/java/com/example/store/repository/OrderRepository.java
@@ -4,4 +4,8 @@ import com.example.store.entity.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findOrderById(long id);
+}
